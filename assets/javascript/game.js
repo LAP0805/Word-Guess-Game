2b5e44e0230359431d7a667d9ede7a21a2982069
Janxxy <lesley.pond@gmail.com>
2018-12-18 11:43:48 -0500

    more logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,13 @@
 
 
 
-var answers = ["hermione", "voldemort", "malfoy", "snape", "hagrid", "luna", "umbridge", "slughorn"]
+var answers = [["m","a","l","f","o","y"], 
+                ["s","n","a","p","e"],
+                ["h","a","g","r","i","d"],
+                ["l","u","n","a"],
+                ["u","m","b","r","i","d","g","e"],
+                ["s","l","u","g","h","o","r","n"]];
 
-var slats = [];
 
 var blankSlate = [];
 
@@ -16,10 +20,25 @@ console.log(randomCharacter);
 
 //fill empty array with _'s length of randomCharacter name//
 for(var i = 0; i < randomCharacter.length; i ++){
-    blankSlate.push("_");
-
+       for(var j = 0; j < randomCharacter[i].length; j ++){
+           blankSlate.push(" _");
+       }
+    
 }
 console.log(blankSlate);
+
+//FUNCTION winning message//
+function arraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+            console.log("you win!")
+    }
+
+    return true;
+}
 
 //FUNCTION
 
@@ -30,63 +49,83 @@ document.onkeyup = function(event) {
         //display correct letter in blankSlate array// alert user if all letters guessed
        if(randomCharacter[0] === letter){
           blankSlate.splice(0,1,letter);
-          console.log(blankSlate);}
+          console.log(blankSlate);
+          console.log("Guess another letter");
+        }
         else if (randomCharacter[1]=== letter){
             blankSlate.splice(1,1,letter);
-            console.log(blankSlate);   
+            console.log(blankSlate);
+            console.log("Guess another letter"); 
         }
         else if (randomCharacter[2] ===letter){
             blankSlate.splice(2,1,letter);
-            console.log(blankSlate);   
+            console.log(blankSlate); 
+            console.log("Guess another letter"); 
         }
         else if (randomCharacter[3] === letter){
             blankSlate.splice(3,1,letter);
             console.log(blankSlate);  
+            console.log("Guess another letter");
         }
         else if (randomCharacter[4] === letter){
             blankSlate.splice(4,1,letter);
             console.log(blankSlate);  
+            console.log("Guess another letter");
         }
         else if (randomCharacter[5] === letter){
             blankSlate.splice(5,1,letter);
             console.log(blankSlate);  
+            console.log("Guess another letter");
         }
         else if (randomCharacter[6] === letter){
             blankSlate.splice(6,1,letter);
             console.log(blankSlate);  
+            console.log("Guess another letter");
         }
         else if (randomCharacter[7] === letter){
             blankSlate.splice(7,1,letter);
             console.log(blankSlate);  
+            console.log("Guess another letter");
         }
         else if (randomCharacter[8] === letter){
             blankSlate.splice(8,1,letter);
-            console.log(blankSlate);  
+            console.log(blankSlate); 
+            console.log("Guess another letter"); 
         }
       //tell user how many wrong guesses they have left, and alert if game is lost
        else{ 
           console.log("wrong");
          var remainingGuesses = wrongGuesses --; 
          console.log("you have " + remainingGuesses + " guesses left");
+         console.log("Guess another letter");
          if (remainingGuesses === 0){
              console.log("You lose!");
          }
         }
+    arraysEqual(blankSlate, randomCharacter);
     }
-
     //END FUNCTION//
-  
+   
+
+    //winning message//
     
+        
+    
+for (var k = 0; k < randomCharacter.length; k ++)
 
 
-
-
-
-    // var my_array = ["baseball", "basketball", "tennis", "golf"];
-    // var start_index = 1
-    // var number_of_elements_to_remove = 2;
-    // var removed_elements = my_array.splice(start_index, number_of_elements_to_remove, "boxing", "bowling", "volleyball");
-    // console.log(removed_elements);
-    // //["tennis", "golf"]
-    // console.log(my_array);
-    // //["baseball", "boxing", "bowling", "volleyball", "golf"];
+    if(randomCharacter[k] === letter){
+        blankSlate.splice(k,1,letter);
+        console.log(blankSlate);
+        console.log("Guess another letter");
+    }
+    //tell user how many wrong guesses they have left, and alert if game is lost
+     else{ 
+        console.log("wrong");
+       var remainingGuesses = wrongGuesses --; 
+       console.log("you have " + remainingGuesses + " guesses left");
+       console.log("Guess another letter");
+       if (remainingGuesses === 0){
+           console.log("You lose!");
+       }
+      }

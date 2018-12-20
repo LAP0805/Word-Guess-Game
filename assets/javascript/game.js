5a567c4e4d3eee2175b97faa6ab066864dacefa1
Janxxy <lesley.pond@gmail.com>
2018-12-19 19:31:25 -0500

    basic functioning logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,35 @@
 
 
 
-var answers = [["m","a","l","f","o","y"], 
-                ["s","n","a","p","e"],
-                ["h","a","g","r","i","d"],
-                ["l","u","n","a"],
-                ["u","m","b","r","i","d","g","e"],
-                ["s","l","u","g","h","o","r","n"]];
+var answers = ["malfoy",
+                "snape",
+                "hagrid",
+                "luna",
+                "umbridge",
+                "slughorn",
+                "dumbledore",
+                "voldemort",
+                "bellatrix",
+                "lovegood",
+                "longbottom",
+                "pettigrew",
+                "severus",
+                "mcgonagall",
+                "nymphadora",
+                "trelawney",
+                "grindelwald",
+                "neville",
+                "lucius",
+                "hermione"];
 
 
 var blankSlate = [];
-
-var wrongGuesses = 4;
+var wrongGuesses = [];
+var rightGuesses = [];
 
 //generate random Harry Potter character//
 var randomCharacter = answers[Math.floor(Math.random()*answers.length)];
-
+var randomCharacterArray = [];
 console.log(randomCharacter);
 
 //fill empty array with _'s length of randomCharacter name//
@@ -27,105 +41,51 @@ for(var i = 0; i < randomCharacter.length; i ++){
 }
 console.log(blankSlate);
 
-//FUNCTION winning message//
-function arraysEqual(arr1, arr2) {
-    if(arr1.length !== arr2.length)
-        return false;
-    for(var i = arr1.length; i--;) {
-        if(arr1[i] !== arr2[i])
-            return false;
-            console.log("you win!")
+//FUNCTION loser///
+function loser (){
+    if (wrongGuesses.length === 5){
+        console.log("you lose!");
     }
-
-    return true;
 }
+//END FUNCTION
 
-//FUNCTION
+//FUNCTION winner
+function winner(){
+if (blankSlate.length === randomCharacterArray.length){
+    console.log("You win! Congrats!");
+}
+}
+//END FUNCTION
+
+//FUNCTION letters guess
 
 //take in user input//
-document.onkeyup = function(event) {
-    var letter = event.key;
 
-        //display correct letter in blankSlate array// alert user if all letters guessed
-       if(randomCharacter[0] === letter){
-          blankSlate.splice(0,1,letter);
-          console.log(blankSlate);
-          console.log("Guess another letter");
-        }
-        else if (randomCharacter[1]=== letter){
-            blankSlate.splice(1,1,letter);
-            console.log(blankSlate);
-            console.log("Guess another letter"); 
-        }
-        else if (randomCharacter[2] ===letter){
-            blankSlate.splice(2,1,letter);
-            console.log(blankSlate); 
-            console.log("Guess another letter"); 
-        }
-        else if (randomCharacter[3] === letter){
-            blankSlate.splice(3,1,letter);
-            console.log(blankSlate);  
-            console.log("Guess another letter");
-        }
-        else if (randomCharacter[4] === letter){
-            blankSlate.splice(4,1,letter);
-            console.log(blankSlate);  
-            console.log("Guess another letter");
-        }
-        else if (randomCharacter[5] === letter){
-            blankSlate.splice(5,1,letter);
-            console.log(blankSlate);  
-            console.log("Guess another letter");
-        }
-        else if (randomCharacter[6] === letter){
-            blankSlate.splice(6,1,letter);
-            console.log(blankSlate);  
-            console.log("Guess another letter");
-        }
-        else if (randomCharacter[7] === letter){
-            blankSlate.splice(7,1,letter);
-            console.log(blankSlate);  
-            console.log("Guess another letter");
-        }
-        else if (randomCharacter[8] === letter){
-            blankSlate.splice(8,1,letter);
-            console.log(blankSlate); 
-            console.log("Guess another letter"); 
-        }
-      //tell user how many wrong guesses they have left, and alert if game is lost
-       else{ 
-          console.log("wrong");
-         var remainingGuesses = wrongGuesses --; 
-         console.log("you have " + remainingGuesses + " guesses left");
-         console.log("Guess another letter");
-         if (remainingGuesses === 0){
-             console.log("You lose!");
-         }
-        }
-    arraysEqual(blankSlate, randomCharacter);
+    document.onkeyup = function(event) {
+        var letter = event.key;
+    
+    for(var m = 0; m < randomCharacter.length; m ++){
+    if(randomCharacter[m] === letter){
+        blankSlate.splice(m,1,letter); 
+        randomCharacterArray.splice(m,1,letter);
+        rightGuesses.splice(m,1,letter);
+        console.log(rightGuesses);
+        winner();
+           console.log(blankSlate); 
+      }
+    }
+    
     }
     //END FUNCTION//
-   
 
-    //winning message//
+
+    //does blankSlate include all the charsAt of randomCharacter//
+    //if yes display you win//
+    
+    
+
+
+
     
         
     
-for (var k = 0; k < randomCharacter.length; k ++)
-
-
-    if(randomCharacter[k] === letter){
-        blankSlate.splice(k,1,letter);
-        console.log(blankSlate);
-        console.log("Guess another letter");
-    }
-    //tell user how many wrong guesses they have left, and alert if game is lost
-     else{ 
-        console.log("wrong");
-       var remainingGuesses = wrongGuesses --; 
-       console.log("you have " + remainingGuesses + " guesses left");
-       console.log("Guess another letter");
-       if (remainingGuesses === 0){
-           console.log("You lose!");
-       }
-      }
---
+++ assets/javascript/game2.js
@@ -0,0 +1,122 @@
+
+
+
+var answers = ["malfoy",
+                "snape",
+                "hagrid",
+                "luna",
+                "umbridge",
+                "slughorn",
+                "dumbledore",
+                "voldemort",
+                "bellatrix",
+                "lovegood",
+                "longbottom",
+                "pettigrew",
+                "severus",
+                "mcgonagall",
+                "nymphadora",
+                "trelawney",
+                "grindelwald",
+                "neville",
+                "lucius",
+                "hermione"];
+
+
+var blankSlate = [];
+var wrongGuesses = [];
+var rightGuesses = [];
+var guessesLeft = 5;
+var wins = 0;
+var losses = 0;
+
+
+
+//generate random Harry Potter character//
+function generateRandomCharacter (){
+randomCharacter = answers[Math.floor(Math.random()*answers.length)];
+console.log(randomCharacter);
+}
+
+//fill empty array with _'s length of randomCharacter name//
+function generateBlankSlate(){
+for(var i = 0; i < randomCharacter.length; i ++){
+       for(var j = 0; j < randomCharacter[i].length; j ++){
+           blankSlate.push(" _");
+       }
+}
+}
+
+//FUNCTION reset game
+function resetGame(){
+    wrongGuesses = [];
+    rightGuesses = [];
+    guessesLeft = 5;
+    generateRandomCharacter();
+    charactersAlreadyPlayed();
+    blankSlate = [];
+    generateBlankSlate();
+    console.log(blankSlate);
+
+}
+//END FUNCTION
+
+//FUNCTION to block characters already displayed from being displayed again//
+function charactersAlreadyPlayed(){
+    
+}
+
+//END FUNCTION//
+
+////FUNCTION guess correct or not
+function mainGame (){
+    correctGuess = false;
+for(var i = 0; i < randomCharacter.length; i ++){
+    if (randomCharacter[i] === letter){ 
+        
+        rightGuesses.push(letter);
+        console.log("correct guesses: " + rightGuesses);
+        blankSlate.splice(i,1,letter);
+        console.log(blankSlate);
+         correctGuess = true;
+    }
+}
+
+
+if (correctGuess === false){
+    console.log("wrong");
+    wrongGuesses.push(letter);
+    guessesLeft --;
+    console.log("wrong guesses: " + wrongGuesses + " Guesses left: " + guessesLeft);
+}
+if (rightGuesses.length === randomCharacter.length){
+    wins ++;
+    console.log("you win!");
+    console.log("wins: " + wins + " losses: " + losses);
+    console.log("_______________________");
+    resetGame();
+} 
+if (guessesLeft === 0){
+    losses ++;
+    console.log("you lose!");
+    console.log("wins: " + wins + " losses: " + losses);
+    console.log("_______________________");
+    resetGame();
+    
+}
+}
+//END FUNCTION
+
+resetGame();
+
+///actual game///
+document.onkeyup = function myGame (event){
+         letter = event.key;
+         mainGame();
+         
+
+    }
+
+//END FUNCTION//
+
+
